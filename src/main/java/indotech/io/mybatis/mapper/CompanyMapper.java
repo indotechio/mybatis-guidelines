6a9d4c2c49d35abf3a494f09517ccc04adb6b75d
Arif Kurniawan <arif.kurniawan@multidayaintegra.com>
2024-03-30 00:32:58 +0700

    Remove @Repostiroy annotation from mapper

--- src/main/java/indotech/io/mybatis/mapper/CompanyMapper.java
+++ src/main/java/indotech/io/mybatis/mapper/CompanyMapper.java
@@ -3,13 +3,11 @@ package indotech.io.mybatis.mapper;
 import indotech.io.mybatis.model.dto.CompanyDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @Mapper
-@Repository
 public interface CompanyMapper {
     List<CompanyDTO> getCompanyList(@Param("offset") int offset, @Param("limit") int limit, @Param("sortField") String sortField, @Param("sortOrder") String sortOrder);
     Integer countCompany();
